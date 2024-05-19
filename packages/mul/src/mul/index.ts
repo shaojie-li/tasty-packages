78a6c2b2f692767af8443cee3cb6eb6bdf6807fe
leeshaojie <shaojie.li@imaginelabs.space>
2024-05-19 15:46:36 +0800

    init commit22

--- packages/mul/src/mul/index.ts
+++ packages/mul/src/mul/index.ts
@@ -3,5 +3,5 @@ import { log } from "../utils/log";
 export function mul(a: number, b: number) {
     const obj = {name: 'mul'}
     log('mul');
-    return a * b + obj.name + '-c1mul';
+    return a * b + obj.name + '-wc1mul';
 }
