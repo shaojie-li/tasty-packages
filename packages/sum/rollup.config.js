532e7e0892db0c155024b9fe02615fa2505b508e
leeshaojie <shaojie.li@imaginelabs.space>
2024-05-19 21:45:32 +0800

    init commit23

--- packages/sum/rollup.config.js
+++ packages/sum/rollup.config.js
@@ -1,25 +1,38 @@
 import typescript from '@rollup/plugin-typescript';
 import delDir from 'rollup-plugin-delete'
-import generatePackageJson from 'rollup-plugin-generate-package-json'
+import generatePackageJson from 'rollup-plugin-generate-package-json';
+import { dts } from 'rollup-plugin-dts'
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import { main, module } from "./package.json";
 
 /**
- * @type {import('rollup').RollupOptions}
+ * @type {import('rollup').RollupOptions[]}
  */
-export default {
-    input: 'src/index.ts',
-    output:  [{
-        file: main,
-        format: "cjs",
+export default [
+    {
+        input: 'src/index.ts',
+        output:  [{
+            file: main,
+            format: "cjs",
+            sourcemap: true,
+        },
+        {
+            file: module,
+            format: "esm",
+            sourcemap: true,
+        }],
+        plugins: [
+            delDir({targets: ["dist/*"]}), 
+            typescript({
+                tsconfig: "./tsconfig.json",
+            }),
+            peerDepsExternal(),
+            
+        ]
     },
     {
-        file: module,
-        format: "esm",
-        exports: "named",
-    }],
-    plugins: [delDir({targets: ["dist/*"]}), typescript({tsconfig: 'tsconfig.json'}), generatePackageJson({
-        baseContents: ({ scripts, ...props }) => ({
-            ...props
-        })
-    })]
-};
+        input: "dist/esm/types/index.d.ts",
+        output: [{ file: "dist/index.d.ts", format: "esm" }],
+        plugins: [dts()],
+    }
+];
